9b6f734b996ea528c4861ed00112dbf45d25ccf1
tarde.cetafaju <tarde.cetafaju@PC03LAB2519>
2024-01-31 17:06:14 -0300

    Adicionar arquivos de projeto.

---
+++ carrosRegistro/Estacionamento.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace carrosRegistro
+{
+    internal class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora+= precoHora;
+
+        }
+        public void adicionarVeiculo()
+        {
+            Console.Write("Digite a placa do veiculo: ");
+            string placa = Console.ReadLine();
+
+            if(veiculos.Any(x => x.Equals(placa.ToUpper())))
+            {
+                Console.WriteLine("Já existe um veículo com essa placa.");
+            }
+            else
+            {
+                veiculos.Add(placa.ToUpper());
+                Console.WriteLine("Veículo cadastrado com sucesso");
+            }
+        }
+        public void removerVeiculo()
+        {
+            Console.Write("Digite a placa que deseja remover: ");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo ficou");
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal precoTotal = precoInicial + precoHora * horas;
+                veiculos.Remove(placa.ToUpper());
+                Console.WriteLine($"O veículo de '{placa.ToUpper()}' foi removido e deve pagar: {precoTotal} reais");
+
+            }
+
+            else
+            {
+                Console.WriteLine("O veiculo não foi encontrado! :( ");
+
+            }
+        }
+        public void listaVeiculo()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veiculos adicionados foi: ");
+                foreach (var v in veiculos)
+                {
+                    Console.WriteLine(v);
+                }
+                    
+
+            }
+            else
+            {
+                Console.WriteLine("Não existe veículos no estacionamento");
+            }
+
+        }
+
+        /*public override string ToString()
+        {
+            return $"";
+        }
+        */
+        //Daria pra por aqui uma listinha de informações:
+        //Veiulos adicionados: 
+        
+    }
+}
---
+++ carrosRegistro/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace carrosRegistro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal precoInicial = 0;
+            decimal precoHora = 0;
+
+            Console.WriteLine("Seja bem vindo ao Estacionamento Fox ");
+            Console.WriteLine("----------------------------------------- ");
+
+            Console.WriteLine("Digite o preço inicial");
+            precoInicial = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o preço por hora");
+            precoHora = Convert.ToInt32(Console.ReadLine());
+
+            Estacionamento es = new Estacionamento(precoInicial, precoHora);
+
+            bool menu = true;
+
+            while (menu)
+            {
+                Console.Clear();
+                Console.WriteLine("Escolha uma opção");
+                Console.WriteLine("1 - Adicionar Veículos");
+                Console.WriteLine("2 - Remover Veículos");
+                Console.WriteLine("3 - Listar Veículos");
+                Console.WriteLine("4 - Sair do sistema");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        es.adicionarVeiculo();
+                        break;
+
+                    case "2":
+                        es.removerVeiculo();
+                        break;
+
+                    case "3":
+                        es.listaVeiculo();
+                        break;
+
+                    case "4":
+                        menu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Digite um opção válida");
+                        break;
+                }
+                Console.WriteLine("Digite uma tecla pra continuar");
+                Console.ReadLine();
+            }
+            Console.WriteLine("Fim !");
+        }
+    }
+}
